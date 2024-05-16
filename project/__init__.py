de71829f30c541eb07d52bb721c3e010f00e8b01
JoddieMQ <joshua.dong@students.mq.edu.au>
2024-05-16 13:09:53 +1000

    Test push

--- project/__init__.py
+++ project/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+# Hello
+
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
